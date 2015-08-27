1b2acf9a1800b9a4bf381f6baa1d9890c9eb28dc
Nicholas Fleck <nicholasfleck@gmail.com>
2015-08-27 02:05:58 -0500

    Buffer pool concept

---
+++ HAARP/BufferPool.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace HAARP
+{
+    internal static class BufferPool
+    {
+        public const int DefaultInitialCount = 5;
+        public const int DefaultInitialBufferSize = 220500;
+
+        private static int _initialCount = DefaultInitialCount;
+        private static int _initialBufferSize = DefaultInitialBufferSize;
+
+        private static readonly object syncObj = new object();
+        private static List<float[]> _buffers;
+        private static readonly HashSet<float[]> _loans = new HashSet<float[]>();
+
+        public static int InitialCount
+        {
+            get { return _initialCount; }
+            set
+            {
+                _initialCount = value < 1 ? 1 : value;
+            }
+        }
+
+        public static int InitialBufferSize
+        {
+            get { return _initialBufferSize; }
+            set
+            {
+                _initialBufferSize = value < 0 ? 0 : value;
+            }
+        }
+
+        private static void Prepare()
+        {
+            if (_buffers != null) return;
+            _buffers = new List<float[]>();
+            for (int i = 0; i < _initialCount; i++)
+            {
+                _buffers.Add(new float[_initialBufferSize]);
+            }
+        }
+
+        public static float[] Loan()
+        {
+            lock (syncObj)
+            {
+                Prepare();
+                foreach (var buffer in _buffers)
+                {
+                    if (_loans.Contains(buffer)) continue;
+                    _loans.Add(buffer);
+                    return buffer;
+                }
+
+                var bufferNew = new float[_initialBufferSize];
+                _buffers.Add(bufferNew);
+                _loans.Add(bufferNew);
+                return bufferNew;
+            }
+        }
+
+        public static void Reclaim(float[] buffer)
+        {
+            lock (syncObj)
+            {
+                Prepare();
+                _loans.Remove(buffer);
+            }
+        }
+    }
+}
---
+++ HAARP/Synthesizer.cs
@@ -0,0 +1,29 @@
+﻿namespace HAARP
+{
+    internal abstract class Synthesizer
+    {
+        public const int MaxSampleRate = 192000;
+        public const int MinSampleRate = 8000;
+        public const int DefaultSampleRate = 44100;
+
+        private int _sampleRate = DefaultSampleRate;
+        private int _position = 0;
+
+        public int SampleRate
+        {
+            get { return _sampleRate; }
+            set
+            {
+                _sampleRate = value < MinSampleRate ? MinSampleRate : value > MaxSampleRate ? MaxSampleRate : value;
+            }
+        }
+
+        public int Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public float TimePosition => (float)_position / _sampleRate;
+    }
+}
