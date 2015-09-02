662024fdc062de5c939b02097517b8b65210348b
Saša Barišić <cartman300@net.hr>
2015-09-02 06:05:17 +0200

    float[] to sound

--- Plybck/Program.cs
+++ Plybck/Program.cs
@@ -26,7 +26,8 @@ namespace Plybck
 			Src.Dispose();
 		}
 
-		public static MemoryStream Create(int SampleRate, double Freq, double Magnitude, uint LenMS, Func<double, double> Func)
+		public static MemoryStream Create(Func<double, double> Func, uint LenMS,
+			int SampleRate = 44100, double Freq = 120, double Magnitude = 1)
 		{
 			int SampleCount = (int)((double)SampleRate * ((double)LenMS / 1000.0f));
 			short[] Buff = new short[SampleCount];
@@ -44,13 +45,32 @@ namespace Plybck
 			return new MemoryStream(Wav.PrependHeader(Ret, SampleRate));
 		}
 
+		public static MemoryStream Create(float[] Samples,
+			int SampleRate = 44100, double Freq = 120, double Magnitude = 1)
+		{
+			int SampleCount = Samples.Length;
+			short[] Buff = new short[SampleCount];
+			byte[] Ret = new byte[Buff.Length * sizeof(short)];
+			double Step = Math.PI * 2.0d / Freq;
+			double Current = 0;
+
+			for (int i = 0; i < Buff.Length; i++)
+			{
+				Buff[i] = (short)(Samples[i] * Magnitude * short.MaxValue);
+				Current += Step;
+			}
+
+			Buffer.BlockCopy(Buff, 0, Ret, 0, Ret.Length);
+			return new MemoryStream(Wav.PrependHeader(Ret, SampleRate));
+		}
+
 		static void Main(string[] args)
 		{
 
-			MemoryStream Snd = Create(44100, 120, 1.0, 1000, (Cur) =>
-			{
-				return Math.Sin(Cur);
-			});
+			float[] Samples = new float[(int)(44100 * 0.8)];
+			for (int i = 0; i < Samples.Length; i++)
+				Samples[i] = (float)Math.Sin((float)i / 10);
+			MemoryStream Snd = Create(Samples);
 
 			FileStream FS = File.Create("test.wav");
 			Snd.CopyTo(FS);
