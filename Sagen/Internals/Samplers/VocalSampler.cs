f336a311c6cbfae6bc8f1f44f0a17d0a1a9dcd47
Nicholas Fleck <nicholasfleck@gmail.com>
2017-02-06 13:28:50 -0600

    Fix the horrible whitespace

--- Sagen/Internals/Samplers/VocalSampler.cs
+++ Sagen/Internals/Samplers/VocalSampler.cs
@@ -8,26 +8,26 @@ namespace Sagen.Internals.Samplers
     /// Handles filtering, voicing/noise synthesis, and articulation given a pre-generated timeline of phonation events.
     /// </summary>
 	internal unsafe class VocalSampler : Sampler
-	{
-		private double sampleIn, sampleOut;
+    {
+        private double sampleIn, sampleOut;
 
-		private readonly BandPassFilter[] bands;
-		private readonly ButterworthFilter lowPass, highPass;
-		private readonly int numBands;
+        private readonly BandPassFilter[] bands;
+        private readonly ButterworthFilter lowPass, highPass;
+        private readonly int numBands;
 
         // Constants which control the attenuation of each individual formant
-		private const double LEVEL_F1 = .02500;
-		private const double LEVEL_F2 = .03800;
-		private const double LEVEL_F3 = .02200;
-		private const double LEVEL_F4 = .00150;
-		private const double LEVEL_F5 = .00025;
-        
+        private const double LEVEL_F1 = .02500;
+        private const double LEVEL_F2 = .03800;
+        private const double LEVEL_F3 = .02200;
+        private const double LEVEL_F4 = .00150;
+        private const double LEVEL_F5 = .00025;
+
         // Resonance levels for each formant
-		private const double RES_F1 = .22;
-		private const double RES_F2 = .08;
-		private const double RES_F3 = .09;
-		private const double RES_F4 = .2;
-		private const double RES_F5 = .23;
+        private const double RES_F1 = .22;
+        private const double RES_F2 = .08;
+        private const double RES_F3 = .09;
+        private const double RES_F4 = .2;
+        private const double RES_F5 = .23;
 
         private const double Height = .25;
         private const double Backness = 0;
@@ -39,7 +39,7 @@ namespace Sagen.Internals.Samplers
 
 
         public VocalSampler(Synthesizer synth, Phoneme vowel) : base(synth)
-		{
+        {
             double f1 = 0.0;
             double f2 = 0.0;
             double f3 = 0.0;
@@ -55,29 +55,29 @@ namespace Sagen.Internals.Samplers
 
             Console.WriteLine($"{f1}, {f2}, {f3}, {f4}");
 
-			bands = new[]
-			{ 
-				new BandPassFilter(f4, f4, synth.SampleRate, RES_F4, RES_F4) { Volume = LEVEL_F4 * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness) },
-				new BandPassFilter(f3, f3, synth.SampleRate, RES_F3, RES_F3) { Volume = LEVEL_F3 * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness) },
-				new BandPassFilter(f2, f2, synth.SampleRate, RES_F2, RES_F2) { Volume = LEVEL_F2 },
-				new BandPassFilter(f1, f1, synth.SampleRate, RES_F1, RES_F1) { Volume = LEVEL_F1 },
+            bands = new[]
+            {
+                new BandPassFilter(f4, f4, synth.SampleRate, RES_F4, RES_F4) { Volume = LEVEL_F4 * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness) },
+                new BandPassFilter(f3, f3, synth.SampleRate, RES_F3, RES_F3) { Volume = LEVEL_F3 * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness) },
+                new BandPassFilter(f2, f2, synth.SampleRate, RES_F2, RES_F2) { Volume = LEVEL_F2 },
+                new BandPassFilter(f1, f1, synth.SampleRate, RES_F1, RES_F1) { Volume = LEVEL_F1 },
             };
 
-			lowPass = new ButterworthFilter(500, synth.SampleRate, PassFilterType.LowPass, .15f);
-			highPass = new ButterworthFilter(6500, synth.SampleRate, PassFilterType.HighPass, .2f);
+            lowPass = new ButterworthFilter(500, synth.SampleRate, PassFilterType.LowPass, .15f);
+            highPass = new ButterworthFilter(6500, synth.SampleRate, PassFilterType.HighPass, .2f);
 
-			numBands = bands.Length;
-		}
+            numBands = bands.Length;
+        }
 
         private static double HeadSizeToFormantOffset(double headSize)
         {
             return -150.0 * Math.Log(headSize, 2.0);
         }
 
-		public override void Update(ref double sample)
-		{
-			synth.Fundamental -= 20.0f * synth.TimeStep;
-			sampleOut = 0f;
+        public override void Update(ref double sample)
+        {
+            synth.Fundamental -= 20.0f * synth.TimeStep;
+            sampleOut = 0f;
 
             // ha ha ha ha ha ha
             //double m = (Math.Sin(synth.TimePosition * Math.PI * 8.0f) + 1.0f) / 2.0f;
@@ -89,17 +89,17 @@ namespace Sagen.Internals.Samplers
 
             // Update filters
             for (int i = 0; i < numBands; i++)
-			{
-				bands[i].Update(sampleIn);
-				sampleOut += bands[i].Value;
-			}
+            {
+                bands[i].Update(sampleIn);
+                sampleOut += bands[i].Value;
+            }
 
-			lowPass.Update(sampleIn);
-			highPass.Update(sampleIn);
-			sampleOut += lowPass.Value * .13f;
-			sampleOut += highPass.Value * .02f * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness);
+            lowPass.Update(sampleIn);
+            highPass.Update(sampleIn);
+            sampleOut += lowPass.Value * .13f;
+            sampleOut += highPass.Value * .02f * Util.Lerp(1.0, BacknessF34AttenuationFactor, Backness);
 
-			sample = sampleOut;
-		}
-	}
+            sample = sampleOut;
+        }
+    }
 }
